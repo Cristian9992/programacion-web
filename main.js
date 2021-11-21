a2a988aa065f8d6405faea12f4497df50ad3e2d9
Cristian9992 <94768198+Cristian9992@users.noreply.github.com>
2021-11-20 20:35:44 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.news-cardss'),{delay: 500 };
+ScrollReveal().reveal('.news-cards'),{delay: 500 };
+ScrollReveal().reveal('.showcase'),{delay: 500 };
+ScrollReveal().reveal('.cards-banner-one'),{delay: 500 };
+ScrollReveal().reveal('showcase'),{delay: 500 };
